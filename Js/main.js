e0cc14f4d9b59b905e2d5974005d556fea3d0b6f
RailJannatov <railcennetov@gmail.com>
2021-10-07 16:36:06 +0400

    Js-task-update

--- Js/main.js
+++ Js/main.js
@@ -47,4 +47,17 @@ let calculator  = (firstNumber,secondNumber,operator)=>{
 }
 console.log(calculator(25,4,"*"));
 
+//factorial
+function factorial(number){
+    if(number === 0 || number === 1){
+        return 1;
+    }
+    
+    for(let i = number - 1; i > 0; i--){
+       number = number * i;
+    }
+    return number;
+}
+console.log(factorial(6));
+
 
